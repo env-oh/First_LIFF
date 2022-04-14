8b3664bfd3da4545d05de48d0827d26b395aa39a
env-oh <natee@envisionthailand.com>
2022-04-14 10:17:23 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-c3wa9m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-c3wa9m)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+//appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const body = document.getElementById('body');
+const userId = document.getElementById('userid');
+const displayName = document.getElementById('displayName');
+const statusMessage = document.getElementById('statusMessage');
+const email = document.getElementById('email');
